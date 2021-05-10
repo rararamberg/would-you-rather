67bad35bb80db0c4e87252de6b18ffefbf6520be
Rachel Ramberg <rachelramberg@Rachels-MacBook-Pro.local>
2021-05-10 16:21:36 -0500

    started playing with showLess showMore on results divs for visual reference

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,14 +1,14 @@
 // import axios from 'axios';
 // import { commentsBaseURL, config } from '../services';
-// import { useEffect, useState } from 'react';
+import { useState } from "react";
 import CommentFeed from "./CommentFeed";
 import CommentForm from "./CommentForm";
 
-
-// pass props to map through question 
+// pass props to map through question
 function Results(props) {
+  const [showMore, setShowMore] = useState(false);
   // const [comments, setComments] = useState([]);
-  
+
   // useEffect(() => {
   //   const fetchComments = async () => {
   //     const resp = await axios.get(commentsBaseURL, config)
@@ -22,7 +22,6 @@ function Results(props) {
 
   // create function that changes vote A and B to percentages
   // PMVP : use a pie graph to display percentages
-  
 
   return (
     <main className="results-main">
@@ -32,41 +31,79 @@ function Results(props) {
       {/* will i need to create a new component for this div / its functionality */}
       {/* click or tap to show more and display comments section */}
       <div className="results-container">
-      {props.questions.map((question) => (
-        <div key={question.id} className="question-result">
-          <h3>WYR</h3>
-          <h4>comment id: {question.fields.commentId}</h4>
-          <h5>(OptionA) {question.fields.optionA} or (OptionB) {question.fields.optionB}</h5>
-          {/* use percentages to display */}
-          <p>(voteA) {question.fields.voteA} vs. (voteB) {question.fields.voteB}</p>
-          {/* insert comment feed component */}
-          {/* insert comment form */}
-          {/* {props.comments.map((comment) => (
-            <div key={question.id} className="comments-section"> 
-            <CommentForm question={question} comment={comment}/>
-            <CommentFeed question={question} comment={comment} />
-            </div>
-          ))} */}
-          <div className="comments-section">
-            {props.comments.map((comment) => {
-              if (comment.fields.questionId === question.fields.commentId) {
-                return (
-                  <div key={comment.id} className="comment"> 
-                  <CommentFeed comment={comment} />
-                  </div>
-                )
-              }
-            })}
-            <CommentForm question={question} setToggleFetch={props.setToggleFetch} />
-          </div>
-          {/* end comments section div */}
-        </div>
-        // end question result div
-      ))}
+        {props.questions.map((question) => {
+          if (showMore === true) {
+            return (
+              <div key={question.id} className="question-result">
+                <h3>WYR</h3>
+                <h4>comment id: {question.fields.commentId}</h4>
+                <h5>
+                  (OptionA) {question.fields.optionA} or (OptionB){" "}
+                  {question.fields.optionB}
+                </h5>
+                {/* use percentages to display */}
+                <p>
+                  (voteA) {question.fields.voteA} vs. (voteB){" "}
+                  {question.fields.voteB}
+                </p>
+                {/* insert comment feed component */}
+                {/* insert comment form */}
+                <div className="comments-section">
+                  {props.comments.map((comment) => {
+                    if (
+                      comment.fields.questionId === question.fields.commentId
+                    ) {
+                      return (
+                        <div key={comment.id} className="comment">
+                          <CommentFeed comment={comment} />
+                        </div>
+                      );
+                    }
+                  })}
+                  <CommentForm
+                    question={question}
+                    setToggleFetch={props.setToggleFetch}
+                  />
+                </div>
+                {/* end comments section div */}
+                <button
+                  onCLick={() => {
+                    setShowMore(!showMore);
+                  }}
+                >
+                  Show Less
+                </button>
+              </div>
+              // end question result div
+            );
+          } else {
+            return (
+              <div key={question.id} className="question-result">
+                <h3>WYR</h3>
+                <h4>comment id: {question.fields.commentId}</h4>
+                <h5>
+                  (OptionA) {question.fields.optionA} or (OptionB){" "}
+                  {question.fields.optionB}
+                </h5>
+                <p>
+                  (voteA) {question.fields.voteA} vs. (voteB){" "}
+                  {question.fields.voteB}
+                </p>
+                <button
+                  onCLick={() => {
+                    setShowMore(!showMore);
+                  }}
+                >
+                  Show More
+                </button>
+              </div>
+            );
+          }
+        })}
       </div>
       {/* end results container div */}
     </main>
-  )
+  );
 }
 
 export default Results;
