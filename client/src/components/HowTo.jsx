4a541827d985af484dbea8ddadf7eb6cbb3dfd47
Rachel Ramberg <rachelramberg@Rachels-MacBook-Pro.local>
2021-05-10 15:03:34 -0500

    added styling for how to page

--- client/src/components/HowTo.jsx
+++ client/src/components/HowTo.jsx
@@ -3,16 +3,26 @@ function HowTo() {
     <main className="how-to">
       <h2>This is the How To Use App page</h2>
       <p>Accessibility info goes here</p>
-      <h3>What is WYR?</h3>
-      <p>
-        Would You Rather? (WYR) is an app that takes a spin off of the popular ice breaker game.
-      </p>
-      <h3>How Do I Play?</h3>
-      <p>
-        To play, you are taken through a module of questions where you must pick which scenario you'd rather go with. Simply click the button of your scenario choice. Instead of getting to know new coworkers or potential contacts at a networking event, you get to play the game at the comfort or your desktop or phone. After your run of questions, you can view results and see where majority lies on which scenarios.
-      </p>
-      <h3>Where the Community At?</h3>
-      <p>In the results section, feel free to join in on the conversation for each question. You can put your comments in the feed by submitting into the form below it.</p>
+      <section className="what-is">
+        <h3>What is WYR?</h3>
+        <p>
+          Would You Rather? (WYR) is an app that takes a spin off of the popular ice breaker game.
+        </p>
+      </section>
+
+      <section className="how-do">
+        <h3>How Do I Play?</h3>
+        <p>
+          To play, you are taken through a module of questions where you must pick which scenario you'd rather go with. Simply click the button of your scenario choice. Instead of getting to know new coworkers or potential contacts at a networking event, you get to play the game at the comfort or your desktop or phone. After your run of questions, you can view results and see where majority lies on which scenarios.
+        </p>
+      </section>
+
+      <section className="where-at">
+        <h3>Where the Community At?</h3>
+        <p>
+          In the results section, feel free to join in on the conversation for each question. You can put your comments in the feed by submitting into the form below it.
+        </p>
+      </section>
     </main>
   )
 }
