4281a1658041ca80437268253071437e2127a66d
Rachel Ramberg <rachelramberg@Rachels-MacBook-Pro.local>
2021-05-12 10:08:40 -0500

    completed linked records and rendering

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -3,20 +3,19 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import CommentFeed from "./CommentFeed";
 import CommentForm from "./CommentForm";
-import Majority from "./Majority"
+import Majority from "./Majority";
 
 function Results(props) {
-  const filterComments = (question) =>
-    props.comments.filter(
-      (comment) => question.fields.commentId === comment.fields.questionId
-    );
+  // const filterComments = (question) =>
+  //   props.comments.filter(
+  //     (comment) => question.fields.commentId === comment.fields.questionId
+  //   );
 
   return (
     <main className="results-main">
       <h2>This is the Results Page</h2>
       <p>Poll results of questions and comments sections go here</p>
       {/* questions.map()to display each Qs results */}
-      {/* will i need to create a new component for this div / its functionality */}
       {/* click or tap to show more and display comments section */}
       <div className="results-container">
         {props.linkedQuestions.map((question) => {
@@ -28,14 +27,13 @@ function Results(props) {
                     <h3>WYR...</h3>
                   </Card.Title>
                   <Card.Subtitle>
-                    {question.fields.optionA}.. OR{" "}.. 
+                    {question.fields.optionA}.. OR ..
                     {question.fields.optionB}
                   </Card.Subtitle>
                   <Card.Text>
                     {/* use percentages to display */}
                     <Majority question={question} />
                   </Card.Text>
-                  {/* <h6> TEMP comment id: {question.fields.commentId}</h6> */}
                   {/* insert comment feed component */}
                   {/* insert comment form */}
                   <Accordion.Toggle
@@ -49,11 +47,16 @@ function Results(props) {
                 <Accordion.Collapse eventKey={question.id}>
                   <Card.Body>
                     <div className="comments-section">
-                      {filterComments(question).map((comment) => (
+                      <div className="comment-feed">
+                        {question.fields.comments.map((comment) => (
+                          <CommentFeed comment={comment} />
+                        ))}
+                      </div>
+                      {/* {filterComments(question).map((comment) => (
                         <div key={comment.id} className="comment">
                           <CommentFeed comment={comment} />
                         </div>
-                      ))}
+                      ))} */}
                       <CommentForm
                         question={question}
                         setToggleFetch={props.setToggleFetch}
