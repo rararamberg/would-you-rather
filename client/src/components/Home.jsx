ac189acf03d7af1e20e8d9e155dade282338fdf5
Rachel Ramberg <rachelramberg@Rachels-MacBook-Pro.local>
2021-05-13 17:09:34 -0500

    attempt collage on gif

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -9,28 +9,45 @@ function Home() {
     {
       src: "https://giphy.com/embed/RJOn3p1xMXDjHiKU5S",
       height: "271"
+    },
+    {
+      src: "https://giphy.com/embed/snEeOh54kCFxe",
+      height: "271"
+    },
+    {
+      src: "https://giphy.com/embed/YVgzWgYgrmEdW",
+      height: "240"
+    },
+    {
+      src: "https://giphy.com/embed/12PIT4DOj6Tgek",
+      height: "361"
+    },
+    {
+      src: "https://giphy.com/embed/bEVKYB487Lqxy",
+      height: "264"
+    },
+    {
+      src: "https://giphy.com/embed/jpbnkneOMU9UeXXCfd",
+      height: "462"
+    },
+    {
+      src: "https://giphy.com/embed/kBZBlLVlfECvOQAVno",
+      height: "361"
+    },
+    {
+      src: "https://giphy.com/embed/lKXEBR8m1jWso",
+      height: "360"
     }
+
   ]
   return (
     <main className="home">
-      <h1>Welcome to WYR!</h1>
       <div className="gif-collage">
         {gifs.map((gif) => (
           <iframe {...gif} frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
         ))}
-
-        <iframe src="https://giphy.com/embed/ZqlvCTNHpqrio" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
-        <iframe src="https://giphy.com/embed/RJOn3p1xMXDjHiKU5S" width="480" height="271" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-        <iframe src="https://giphy.com/embed/snEeOh54kCFxe" width="480" height="271" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-        <iframe src="https://giphy.com/embed/YVgzWgYgrmEdW" width="480" height="240" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-        <iframe src="https://giphy.com/embed/12PIT4DOj6Tgek" width="480" height="361" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-        <iframe src="https://giphy.com/embed/bEVKYB487Lqxy" width="480" height="264" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-        <iframe src="https://giphy.com/embed/jpbnkneOMU9UeXXCfd" width="480" height="462" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-        <iframe src="https://giphy.com/embed/kBZBlLVlfECvOQAVno" width="480" height="361" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-        <iframe src="https://giphy.com/embed/lKXEBR8m1jWso" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
       </div>
       <p><a href="https://giphy.com/gifs/h">via GIPHY</a></p>
-
     </main>
   );
 }
